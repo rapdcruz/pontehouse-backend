d9fbf55e1caf20df2a972e64adbb9e7e14680022
Rafael Cruz <cruz.a.rafael@gmail.com>
2022-07-17 10:49:27 +0100

    message i'm alive

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ const app = express();
 const database = require('./config/database');
 const cors = require("cors");
 
-const authenticationRouter = require('./routes/authentication.router'); 
-const cargoRouter = require('./routes/cargo.router'); 
-const historicoRouter = require('./routes/historico.router'); 
-const produtoRouter = require('./routes/produto.router'); 
-const utilizadorRouter = require('./routes/utilizador.router'); 
+const authenticationRouter = require('./routes/authentication.router');
+const cargoRouter = require('./routes/cargo.router');
+const historicoRouter = require('./routes/historico.router');
+const produtoRouter = require('./routes/produto.router');
+const utilizadorRouter = require('./routes/utilizador.router');
 
 const administrationFilter = require('./middleware/administrationFilter');
 const authenticationFilter = require('./middleware/authenticationFilter');
@@ -23,38 +23,32 @@ app.use(cors({
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*'); 
+  res.header('Access-Control-Allow-Methods', '*');
   res.header('Allow', '*');
   next();
-  });
+});
 
 //Configurações
 app.set('port', process.env.SERVER_PORT || 5001); //Middlewares
 app.use(express.json());
 //Rotas
-app.use('/teste/admin',[authenticationFilter,administrationFilter], (req, res) => {
-    res.send("Rota teste admin.");
-}); 
 
-app.use('/teste/user',[authenticationFilter], (req, res) => {
-  res.send("Rota TESTE user.");
-}); 
-//app.use('/', (req, res) => {
-  //  res.send("Hello World");
-//});
+app.use('/', (req, res) => {
+  res.send("I'm Alive: "+new Date());
+});
 
-app.use('/api/v1/authentication',authenticationRouter);
+app.use('/api/v1/authentication', authenticationRouter);
 app.use('/api/v1/cargo', authenticationFilter, cargoRouter);
-app.use('/api/v1/historico',authenticationFilter, historicoRouter);
+app.use('/api/v1/historico', authenticationFilter, historicoRouter);
 app.use('/api/v1/produto', authenticationFilter, produtoRouter);
-app.use('/api/v1/utilizador',authenticationFilter, utilizadorRouter);
+app.use('/api/v1/utilizador', authenticationFilter, utilizadorRouter);
 
 //app.use('/genero', generoRouter); 
 
 
 app.listen(app.get('port'), () => {
-    console.log('########### -> Syncronizing database...');
-    console.log("########### -> Start server on port " + app.get('port'))
+  console.log('########### -> Syncronizing database...');
+  console.log("########### -> Start server on port " + app.get('port'))
 });
 
 
